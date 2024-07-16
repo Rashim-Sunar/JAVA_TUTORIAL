d453feac588d06b9d171739417daf7c6d3ef72f3
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-07-16 11:05:51 -0400

    Private access modifiers in java and getter and setter method

---
+++ RS_7_ACCESS_MODIFIERS.java
@@ -0,0 +1,37 @@
+/*FOur types of access modifiers
+1. Private    2.Default   3. Protected   4.Public
+*/
+class Employee7{
+    private int id;
+    private String name;
+    //getters and setters access.. 
+    public void setName(String n){
+        this.name = n;
+    }
+
+    public void setId(int ID){
+        this.id = ID;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+}
+
+public class RS_7_ACCESS_MODIFIERS {
+    public static void main(String[] args) {
+        Employee7 e = new Employee7();
+
+        // e.id = 101;
+        // e.name = "aakash";  Throws an error due to private access modfifier... 
+
+        e.setName("Aakash");
+        e.setId(101);
+        System.out.println(e.getName());
+        System.out.println(e.getId());
+    }
+}
