216eb54bd31adc6b8bd02b617cd3e03f65ec3b09
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-08-02 15:35:43 -0400

    User defined exceptio

--- RS_27_Exception_Handling/TestThrow3.java
+++ RS_27_Exception_Handling/TestThrow3.java
@@ -1,7 +1,19 @@
 //Throwing userdefined exception... 
 
+class MyException extends Exception{
+  public MyException(String msg){
+   //Call the constructor of parent Exception...
+    super(msg);
+  }
+}
+
 public class TestThrow3 {
     public static void main(String[] args) {
-        
-    }
+      try{
+	//Throw an object of userdefined exception...
+	throw new MyException("Exception occured in main class. This is a custom exception");
+      }catch(MyException e){ 
+	System.out.println(e.getMessage());	
+      }
+   }
 }
